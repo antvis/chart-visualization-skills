001aa459a3bf70e15573b391b33d8ce41b3d0ae5
逍为 <xiaowei.wzw@antgroup.com>
2026-07-23 18:40:29 +0800

    chore: remove command

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 
 import { Command } from 'commander';
 import path from 'path';
-import { registerRetrieveCommand } from './commands/retrieve';
 
 const pkg = require(path.resolve(__dirname, '../package.json'));
 
@@ -13,8 +12,6 @@ program
   .description('CLI tool for AntV chart visualization docs retrieval')
   .version(pkg.version)
   .option('--debug', 'Show full stack trace on error');
-
-registerRetrieveCommand(program);
 
 try {
   program.parse();
